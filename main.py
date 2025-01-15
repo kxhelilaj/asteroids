bf600aa97ee8d903d8a36cdbcc2347fdbc1fb13d
kejvi <kejvi.xhelilaj@arkevo-group.com>
2025-01-15 10:50:26 +0100

    infinite loop

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while screen:
+    while True:
         for event in pygame.event.get():
              if event.type == pygame.QUIT:
                  return
+        
         screen.fill('black')
         pygame.display.flip()
 
